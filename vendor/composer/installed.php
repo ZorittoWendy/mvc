f4617ed6570266d22de15e1f4cf0ed85078c93d7
zoritto <zoritto@zoritto.Dev>
2024-07-24 22:50:27 -0300

    adicionando o composer, fazendo o controller e view

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'zoritto/mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2a6054cdc83dfd2826f0b0a7b9b061a694381b1f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'zoritto/mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2a6054cdc83dfd2826f0b0a7b9b061a694381b1f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
