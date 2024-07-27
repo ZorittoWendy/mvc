e57b545a60deb53120389c174e386e826afc59a2
zoritto <zoritto@zoritto.Dev>
2024-07-26 22:24:56 -0300

    Adicionando pages em html, Modificação no home controller e page

--- app/Controller/Pages/Home.php
+++ app/Controller/Pages/Home.php
@@ -3,11 +3,24 @@
 namespace App\Controller\Pages;
 
 use App\Utils\View;
+use App\Model\Entity\Organization;
 
-class Home
+class Home extends Page
 {
     public static function getHome()
     {
-        return View::render("pages/home");
+        $obOrganization = new Organization;
+        echo "<pre>";
+        print_r($obOrganization);
+        echo "</pre>";
+        exit;
+
+        $content = View::render("pages/home", [
+            "name" => "ZORITTO - DEV",
+            "description" => "liekdin: www.linkedin.com/in/wendeson-farias",
+            "site" => "www.linkedin.com/in/wendeson-farias",
+        ]);
+
+        return parent::getPage("ZORITTO - canal - Home", $content);
     }
 }
---
+++ app/Model/Entity/Organization.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Model\Entity;
+
+class Organization
+{
+    public $id = 1;
+
+    public $name = "canal - zoritto";
+
+    public $site = "www.linkedin.com/in/wendeson-farias";
+
+    public $description = 'Em um mundo onde 80% da
+    população mundial possuem super poderes, o tímido
+    estudante Izuku Midoriya teve a infelicidade de nascer
+    sem poderes.Cap. 1 Grande fã do sorridente All Might,
+    o herói conhecido como o símbolo da paz, Izuku, sofre
+    com a frustração de saber que jamais terá uma individualidade
+    especial para que possa se tornar, assim como seu grande ídolo,
+    em um defensor dos fracos e oprimidos.[15]
+
+    Mesmo sofrendo bullying por seus amigos de escola, como o
+    arrogante Katsuki Bakugo, o garoto nunca abandonou o herói
+    existente dentro de si. Gentil e generoso, ele está
+    sempre pronto a ajudar quem precisa';
+}
