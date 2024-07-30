aa95a5c5ac17168414acc1f63f6bb2d405d40fc9
zoritto <zoritto@zoritto.Dev>
2024-07-29 21:36:49 -0300

    finalizando o response e criando o Router

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
 
 require __DIR__ . "/vendor/autoload.php";
 
+use App\Http\Router;
 use App\Controller\Pages\Home;
+
+define("URL", "http://localhost/mvc");
+
+$obRouter = new Router(URL);
+echo "<pre>";
+print_r($obRouter);
+echo "</pre>";
+exit();
 
 echo Home::getHome();
