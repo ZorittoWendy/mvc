e4b8c6f5deb6a7e60913e8665852b9e916853702
zoritto <zoritto@zoritto.Dev>
2024-08-01 06:58:01 -0300

    adicionando configuracao de rota na URL

--- app/Http/Router.php
+++ app/Http/Router.php
@@ -2,6 +2,9 @@
 
 namespace App\Http;
 
+use \Closure;
+use \Exception;
+
 class Router
 {
     private $url = "";
@@ -16,5 +19,79 @@ class Router
     {
         $this->request = new Request();
         $this->url = $url;
+        $this->setPrefix();
+    }
+
+    public function setPrefix()
+    {
+        $parseUrl = parse_url($this->url);
+
+        $this->prefix = $parseUrl["path"] ?? "";
+    }
+
+    private function addRoute($method, $route, $params = [])
+    {
+        foreach ($params as $key => $value) {
+            if ($value instanceof Closure) {
+                $params["controller"] = $value;
+                unset($params[$key]);
+                continue;
+            }
+        }
+
+        $patternRoute = "/^" . str_replace("/", "\/", $route) . '$/';
+
+        $this->routes[$patternRoute][$method] = $params;
+    }
+
+    public function get($route, $params = [])
+    {
+        return $this->addRoute("GET", $route, $params);
+    }
+
+    private function getUri()
+    {
+        $uri = $this->request->getUri();
+
+        $xUri = strlen($this->prefix) ? explode($this->prefix, $uri) : [$uri];
+
+        return end($xUri);
+    }
+
+    private function getRoute()
+    {
+        $uri = $this->getUri();
+
+        $httpMethod = $this->request->gethttpMethod();
+        
+        foreach($this->routes as $patternRoute=>$methods)
+        {
+
+            if (preg_match($patternRoute,$uri)) 
+            {
+                if ($methods[$httpMethod])
+                {
+                    return $methods[$httpMethod];
+                }
+
+                throw new Exception("Método não permitido", 405);
+            }
+        }
+
+        throw new Exception("URL não encontrada", 404);
+        
+    }
+
+    public function run()
+    {
+        try {
+            $route = $this->getRoute();
+            echo "<pre>";
+            print_r($route);
+            echo "</pre>";
+            exit();
+        } catch (Exception $e) {
+            return new Response($e->getCode(), $e->getMessage());
+        }
     }
 }
--- index.php
+++ index.php
@@ -3,14 +3,18 @@
 require __DIR__ . "/vendor/autoload.php";
 
 use App\Http\Router;
+use App\Http\Response;
 use App\Controller\Pages\Home;
 
 define("URL", "http://localhost/mvc");
 
 $obRouter = new Router(URL);
-echo "<pre>";
-print_r($obRouter);
-echo "</pre>";
-exit();
 
-echo Home::getHome();
+$obRouter->get("/A", [
+    function () {
+        return new Response(200, Home::getHome());
+    },
+]);
+
+$obRouter->run()
+    ->sendResponse();
