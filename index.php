3296efa3334c6481f2831ec56681d868d6b74a03
zoritto <zoritto@zoritto.Dev>
2024-08-03 19:32:42 -0300

    adicionando as rotas. modificando o index

--- index.php
+++ index.php
@@ -3,18 +3,12 @@
 require __DIR__ . "/vendor/autoload.php";
 
 use App\Http\Router;
-use App\Http\Response;
-use App\Controller\Pages\Home;
 
 define("URL", "http://localhost/mvc");
 
 $obRouter = new Router(URL);
 
-$obRouter->get("/", [
-    function () {
-        return new Response(200, Home::getHome());
-    },
-]);
+include __DIR__.'/routes/pages.php';
 
 $obRouter->run()
     ->sendResponse();
